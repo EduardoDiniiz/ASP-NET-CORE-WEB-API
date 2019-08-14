45127a163a28ecb375a094720de763e521ba42f9
EduardoDiniiz <eduardocco11@gmail.com>
2019-08-13 22:19:19 -0300

    Efetuando envio de arquivo para api

--- src/DevIO.Business/Intefaces/IProdutoRepository.cs
+++ src/DevIO.Business/Intefaces/IProdutoRepository.cs
@@ -10,5 +10,6 @@ namespace DevIO.Business.Intefaces
         Task<IEnumerable<Produto>> ObterProdutosPorFornecedor(Guid fornecedorId);
         Task<IEnumerable<Produto>> ObterProdutosFornecedores();
         Task<Produto> ObterProdutoFornecedor(Guid id);
+        Task<object> ObterProdutoFornecedores();
     }
 }
--- src/DevIO.Data/Repository/ProdutoRepository.cs
+++ src/DevIO.Data/Repository/ProdutoRepository.cs
@@ -19,6 +19,11 @@ namespace DevIO.Data.Repository
                 .FirstOrDefaultAsync(p => p.Id == id);
         }
 
+        public Task<object> ObterProdutoFornecedores()
+        {
+            throw new NotImplementedException();
+        }
+
         public async Task<IEnumerable<Produto>> ObterProdutosFornecedores()
         {
             return await Db.Produtos.AsNoTracking().Include(f => f.Fornecedor)
